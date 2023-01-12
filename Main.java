eae471983846db3ecc08e911375b9d04d684d261
Eris Kociaj <eris.kociaj@elev.ga.ntig.se>
2023-01-12 11:07:43 +0100

    1:a fight commit

--- Main.java
+++ Main.java
@@ -1,9 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args)
     {
-        System.out.print("Test");
-        System.out.print("Test 23");
+        int dinHälsa = 100;
+
+        int aiHälsa = 100;
         
+        boolean game = true;
+
+        while(game)
+        {
+            try
+            {
+                Scanner sc = new Scanner(System.in);
+                int num1 =sc.nextInt();
+
+                if(num1 == 1)
+                {
+                    System.out.println("Du slog din fienden!");
+                    System.out.println(" ");
+
+                    Random skada = new Random();
+
+                    int attack;
+
+                    attack = skada.nextInt(5);
+                    
+                }
+            }
+
+            catch (InputMismatchException e)
+            {
+
+            }
+        }
     }
 }
